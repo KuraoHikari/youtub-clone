c8d43483dfb14aed81dc32a83599a410942a6ad8
KuraoHikari <dewaindra705@gmail.com>
2022-10-02 18:01:00 +0800

    update

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -43,8 +46,73 @@ const Input = styled.input`
   background-color: transparent;
   z-index: 999;
 `;
+const Desc = styled.textarea`
+  border: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+`;
+const Button = styled.button`
+  border-radius: 3px;
+  border: none;
+  padding: 10px 20px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+`;
+const Label = styled.label`
+  font-size: 14px;
+`;
 
 const Upload = ({ setOpen }) => {
+  const [img, setImg] = useState(undefined);
+  const [video, setVideo] = useState(undefined);
+  const [imgPerc, setImgPerc] = useState(0);
+  const [videoPerc, setVideoPerc] = useState(0);
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const handleTags = (e) => {
+    setTags(e.target.value.split(','));
+  };
+
+  const uploadFile = (file) => {
+    const storage = getStorage();
+    const fileName = new Date().getTime() + file.name;
+    const metadata = {
+      contentType: 'image/jpeg',
+    };
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {}
+    );
+  };
+  useEffect(() => {
+    uploadFile(video);
+  }, [video]);
+  useEffect(() => {
+    uploadFile(img);
+  }, [img]);
   return (
     <Container>
       <Wrapper>
@@ -52,8 +120,14 @@ const Upload = ({ setOpen }) => {
           <CloseIcon />
         </Close>
         <Title>Upload a new Video</Title>
-        <Input type="file" accept="video/*" />
-        <Input type="text" placeholder="Title" />
+        <Label>Video:</Label>
+        <Input type="file" accept="video/*" onChange={(e) => setVideo(e.target.files[0])} />
+        <Input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
+        <Desc placeholder="Description" rows={8} onChange={(e) => setDesc(e.target.value)} />
+        <Input type="text" placeholder="Sparate the tags with commas" onChange={(e) => handleTags(e)} />
+        <Label>Image:</Label>
+        <Input type="file" accept="image/*" onChange={(e) => setImg(e.target.files[0])} />
+        <Button>Upload</Button>
       </Wrapper>
     </Container>
   );
